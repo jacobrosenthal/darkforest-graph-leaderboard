1518170e7c66bf8c375338032de9b575865f7bce
Jacob Rosenthal <jacobrosenthal@gmail.com>
2020-12-20 16:52:37 -0700

    someday

--- index2.js
+++ index2.js
@@ -1,94 +1,62 @@
 const fs = require('fs');
+const df = require('../darkforest/something');
 
 const main = async () => {
 
-    let now = Math.trunc(Date.now() / 1000);
-
     let rawdata = fs.readFileSync("planets.json");
-    let owners = new Map();
+    let players = new Map();
 
-    JSON.parse(rawdata).filter(p => p.owner.id !== "0x0000000000000000000000000000000000000000").map(p => updatePlanetToTime(p, now)).forEach(p => {
+    JSON.parse(rawdata)
+        // remove the system player
+        .filter(p => p.owner.id !== "0x0000000000000000000000000000000000000000")
+        // massage the data into just enough of a df looking object
+        .map(p => {
 
-        let old = owners.get(p.owner.id);
-        let values = [];
-        if (old !== undefined) {
-            values = old;
-        }
-        values.push(p);
-        owners.set(p.owner.id, values)
-    });
+            // df functions still calls it energy, and doesnt use 'lazy'
+            p.energy = p.populationLazy;
+            p.silver = p.silverLazy;
+            p.energyCap = p.populationCap;
+            p.energyGrowth = p.populationGrowth
+
+            // df doesnt have 0x on owner fields
+            p.owner = p.owner.id.substring(2, p.owner.id);
+
+            // p.id is 0 padded, no 0x prefix, so same as LocationId?
+            p.locationId = p.id;
+
+        })
+        // update the lazy population and silver to current time
+        .map(p => df.updatePlanetToTime(p, Date.now()))
+        // build a hashmap of array of planets for each owner
+        .forEach(p => {
+
+            let old = players.get(p.owner.id);
+            let values = [];
+            if (old !== undefined) {
+                values = old;
+            }
+            values.push(p);
+            players.set(p.owner.id, values)
+        });
 
     // todo calculate its silverspent on js side to check
 
     let leaderboard = [];
 
-    for (owner of owners.keys()) {
+    // for each players, 10% of their total spent and unspent silver, + their top 10 energyCap
+    for (player of players.keys()) {
 
-        let sorted = owners.get(owner).sort((a, b) => b.populationCap - a.populationCap);
-        let ten = sorted.slice(0, 10).reduce((acc, p) => acc + p.populationCap, 0);
-        let spent = sorted.reduce((acc, p) => acc + p.silverSpentComputed + p.silverLazy, 0) / 10;
+        let sorted = players.get(player).sort((a, b) => b.energyCap - a.energyCap);
+        let ten = sorted.slice(0, 10).reduce((acc, p) => acc + p.energyCap, 0);
+        let spent = sorted.reduce((acc, p) => acc + p.silverSpentComputed + p.silver, 0) / 10;
 
-        leaderboard.push([owner, ten + spent]);
+        leaderboard.push([player, ten + spent]);
     }
 
+    // sort scores and print the top 10
     let topten = leaderboard.sort((a, b) => b[1] - a[1]).slice(0, 10);
     console.log(topten)
 }
 
 
 main()
-
-
-
-//careful ours have 0x on front
-function hasOwner(planet) {
-    return planet.owner !== "0x0000000000000000000000000000000000000000";
-};
-
-function getSilverOverTime(
-    planet,
-    startTimeMillis,
-    endTimeMillis
-) {
-    if (!hasOwner(planet)) {
-        return planet.silverLazy;
-    }
-
-    if (planet.silverLazy > planet.silverCap) {
-        return planet.silverCap;
-    }
-    const timeElapsed = endTimeMillis - startTimeMillis;
-
-    return Math.min(
-        timeElapsed * planet.silverGrowth + planet.silverLazy,
-        planet.silverCap
-    );
-}
-
-
-function getEnergyAtTime(planet, atTimeMillis) {
-    if (planet.populationLazy === 0) {
-        return 0;
-    }
-    if (!hasOwner(planet)) {
-        return planet.populationLazy;
-    }
-    const timeElapsed = atTimeMillis - planet.lastUpdated;
-    const denominator =
-        Math.exp((-4 * planet.populationGrowth * timeElapsed) / planet.populationCap) *
-        (planet.populationCap / planet.populationLazy - 1) +
-        1;
-    return planet.populationCap / denominator;
-}
-
-//careful, altered to remove /1000 as ours is in seconds
-function updatePlanetToTime(planet, atTimeS) {
-    planet.silverLazy = getSilverOverTime(
-        planet,
-        planet.lastUpdated,
-        atTimeS
-    );
-    planet.populationLazy = getEnergyAtTime(planet, atTimeS);
-    planet.lastUpdated = atTimeS;
-    return planet;
-}
